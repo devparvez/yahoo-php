2a09b31a550d7605dfd71c4530df5aa7576b1e18
parvezmahmud-developer <parvezmahmud78@gmail.com>
2020-12-24 09:25:51 +0600

    datatypes

---
+++ datatypes.php
@@ -0,0 +1,16 @@
+<?php 
+$x = "parvez mahmud";
+echo $x ."<br>";
+$x="2345";
+$x= 2345.555;
+$x = false;
+$x = array("html","css","java");
+$x = null;
+//echo $x[0]."<br>";
+echo $x."<br>";
+
+
+var_dump($x);
+
+
+ ?>
