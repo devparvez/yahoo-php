a1408c78a00c748dfdb76bca53cdec0f330fe0a9
parvezmahmud-developer <parvezmahmud78@gmail.com>
2020-12-24 08:23:55 +0600

    variables

---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php 
+	$name= "parvez <br>";
+	$num = 234.5 ."<br>";
+
+	echo $num;
+
+
+	echo $name;
+	echo $name;
+	echo $name;
+	echo $name;
+	echo $name;
+	echo "<h1>". $name ."</h1>";
+	echo "Hello whats up guy:". $name;
+
+
+ ?>
