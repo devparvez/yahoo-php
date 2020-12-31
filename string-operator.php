df75bf8c36b17a05c9e034ed26ed3e54b37ad320
parvezmahmud-developer <parvezmahmud78@gmail.com>
2020-12-31 09:30:04 +0600

    string operator

---
+++ string-operator.php
@@ -0,0 +1,29 @@
+<?php 
+/*$a = "Hello";
+$s = $a . " world";
+
+echo $s;
+*/
+/*
+$a = 2021;
+$s = $a . " world ";
+
+echo $s;
+*/
+/*
+$a = 2021;
+
+$s = $a . " world ". 200;
+
+echo $s;
+*/
+$s = 2021;
+
+$s .=" , Hello this is";
+
+$s .=" Our world";
+
+echo $s;
+
+
+ ?>
