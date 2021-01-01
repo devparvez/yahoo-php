a7c83f26cbedd2d288f9f9cb4e223ce3f5a29c4c
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-01 11:07:06 +0600

    While loop class 19

---
+++ loop.php
@@ -0,0 +1,45 @@
+<?php 
+
+//while loop
+/*
+$a = 1;
+while($a <= 10){
+	echo " Happy new Year <br>";
+	$a = $a + 1;
+}
+*/
+
+/*
+$a = 1;
+
+while($a <= 10){
+
+	echo $a. " ) Happy new Year <br>";
+
+	$a++;
+}
+*/
+
+/*
+$a = 1;
+
+while($a <= 10){
+
+	echo $a. " ) Happy new Year <br>";
+
+	$a++;
+}
+*/
+$a = 1;
+echo "<ul>";
+while($a <= 30){
+	echo "<li>".$a.")Hello<br></li>";
+	$a= $a+2;
+echo "</ul>";
+}
+
+
+
+
+?>
+
