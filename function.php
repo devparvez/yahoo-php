d24743a6b6307887183c49d12827c122bdc98911
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-10 08:57:34 +0600

    Functions with parameters class-26

--- function.php
+++ function.php
@@ -1,7 +1,7 @@
 
 <?php 
 
-function name(){
+/*function name(){
 	echo "my name is parvez ok<br>";
 }
 
@@ -12,6 +12,31 @@ echo "this is my ('') income<br>";
 name();
 name();
 name();
+*/
+/*function hello($name){
+	echo "My name is ".$name ."<br>";
+}
+hello("parvez");
+hello("Aman");
+*/
+function fullName($fname="aman",$lname="ahmed"){
+echo "Hello $fname $lname.<br>";
+
+}
+fullName("Parvez","mahmud");
+fullName("Bill","Gates");
+fullName();
+fullName("tum");
+fullName("tum","hello");
+function sum($a,$b){
+	echo $a + $b;
+}
+sum(30,20.33);
+
+
+
+
+
 
 
 
