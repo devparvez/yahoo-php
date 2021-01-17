25cb4e33780334616fcf07fcc60bf87b37752290
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-17 10:12:04 +0600

    Multidimentional array class35

--- array.php
+++ array.php
@@ -1,44 +1,71 @@
 <?php  
-//for each loop for array and last two classes of array saved in function.php
+//Multidimentional array
 
-/*
-$colors = [
-	"red",
-	"green",
-	"blue"
+/*$emp = [
+	[1,"parvez","manager",50000],
+	[2,"Hossain","salesman",2000],
+	[1,"Saddam","cleaner",1000],
+	[1,"laila","driver",500],
 ];
-foreach($colors as $value){
-	echo $value."<br>";
-}
-
-//foreach loop for associative array
-
-$age = [
-	"bill" => 25,
-	"gates" => 22,
-	"steve" => 20
-
-];
-foreach($age as $value){
-
-	echo $value."<br>";
-}
 */
-//if we want to print values with keys
+/*
 
-$age = [
+echo $emp [0][1]."<br>";
+echo $emp [3][1]."<br>";
+echo "<br>";
+echo $emp[0][0]."   ";
+echo $emp[0][1]."   ";
+echo $emp[0][2]."   ";
+echo $emp[0][3]."   ";
 
-	"bill" => 25,
-	"gates" => 22,
-	"steve" => 20
+echo "<br>";
 
+echo $emp[1][0]."   ";
+echo $emp[1][1]."   ";
+echo $emp[1][2]."   ";
+echo $emp[1][3]."   ";
+
+
+echo "<pre>";
+
+print_r($emp);
+
+echo "</pre>";
+*/
+$emp = [
+	[1,"parvez","manager",50000],
+	[2,"Hossain","salesman",2000],
+	[1,"Saddam","cleaner",1000],
+	[1,"laila","driver",500],
 ];
-foreach($age as $key => $value){
-	"echo <ul>";
+//using for loop to echo multidimentional array
+for($row=0;$row<4;$row++){
+	for($cols = 0; $cols < 4; $cols++){
+		echo $emp[$row][$cols]." ";
+	}
+	echo "<br>";
+};
+//using foreach loop to echo multidimentional array
 
-	echo "<li> $key = $value </li>";
+echo "<table border='2px' cellpadding='10px' cellspacing='10px'>";
+echo "<tr>
 
-	"echo </ul>";
+	<th>Emp id</th>
+	<th>Emp Name</th>
+	<th>Designation</th>
+	<th>Salary</th>
+
+ </tr>";
+
+foreach($emp as $v1){
+
+	echo "<tr>";
+
+foreach($v1 as $v2){
+	echo "<td> $v2 </td> ";
 }
+echo "</tr>";
+}
+echo "</table>";
 
 ?>
