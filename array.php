c9d6e1cfc7e66c6ba6ac231d253c9406f4e726b1
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-21 10:04:45 +0600

    in_array and array_search class-39

--- array.php
+++ array.php
@@ -1,92 +1,42 @@
 <?php
-/*$emp = [
-	[1,"parvez","mana",5000],
-	[2,"moyna","sales",2000],
-	[3,"tada","driver",3000],
-	[4,"aman","help",4000]
 
-];
+$food=array('berry','grapes','banana','apple','grapes',55);
+
+//echo in_array('banana', $food);
+/*
+if(in_array('lime', $food)){
+
+	echo "Find successfully";
+}else{
+
+	echo "can't find";
+}
 */
 
 /*
-$emp = [
-	[
-		"id"=>1,
-		"name"=>"parvez",
-		"designation"=>"Mana",
-		"salary"=>5000
-	],
-	[
-		"id"=>2,
-		"name"=>"moyna",
-		"designation"=>"sales",
-		"salary"=>5000
-	],
-	[
-		"id"=>3,
-		"name"=>"tada",
-		"designation"=>"driver",
-		"salary"=>5000
-	],
-	[
-		"id"=>4,
-		"name"=>"aman",
-		"designation"=>"help",
-		"salary"=>4000
-	]
+if(in_array(55,$food,true)){
 
+	echo "Find successfully";
+}else{
 
-
-
-];
-echo  "<table border='2px' cellpadding='5px' cellspacing='0'>
-<tr>
-<th>emp id</th>
-<th>Name</th>
-<th>Designation</th>
-<th>Salary</th>
-</tr>
-
-";
-foreach($emp as list("id" => $id,"name" => $name,"designation"=>$designation,"salary" => $salary)){
-
-	echo "<tr> <td>$id</td> <td>$name</td> <td>$designation </td><td>$salary</td></tr>";
+	echo "can't find";
 }
-echo "</table>";
 */
-//Array count and sizeof function
 
-//$food=array('berry','grapes','banana','apple');
-
-//echo count($food);
-//echo sizeof($food);
-/*$food = array(
-	'fruits' =>array('berry','grapes','banana','apple'),
-	'vegie'  => array('carrot','collard','pea')
-
-
-);
+/*
+$a = array(array('p','h'),array('p','h'),'o');
+if(in_array(array('p','h'),$a,true)){
+	echo "find successfully";
+}else{
+	echo "can't find";
+}
 */
-//echo count($food);
-//echo sizeof($food);
-//echo count($food,1);
-//echo count($food['fruits'],1);
-$food=array('berry','grapes','banana','apple','grapes');
 
-//$len=count($food);
+//$food=array('berry','grapes','banana','apple','grapes',55);
+ //echo array_search('banana', $food);
 
-//for($i = 0; $i < $len;$i++){
-//	echo $food[$i]."<br>";
-//}
-echo "<pre>";
+$food = array('a' => 'orange', 'b'=>'banana', 'c'=>'apple','d'=>'grapes');
 
-print_r(array_count_values($food));
-
-echo "</pre>";
-
-
-
-
-
+ echo array_search('apple', $food);
 
 ?>
