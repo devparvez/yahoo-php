9c51f5748cd4fbb853a7765b4d9a84f9038c7bf4
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-26 08:43:00 +0600

    array_push array_pop class41

--- array.php
+++ array.php
@@ -1,61 +1,29 @@
 <?php
+//===========pop and push =========
 
-//$food=array('berry','grapes','banana','apple','grapes',55);
+/*$fruit = ["orange","banana","grapes","apple"];
 
-//echo in_array('banana', $food);
-/*
-if(in_array('lime', $food)){
-
-	echo "Find successfully";
-}else{
-
-	echo "can't find";
-}
-*/
-
-/*
-if(in_array(55,$food,true)){
-
-	echo "Find successfully";
-}else{
-
-	echo "can't find";
-}
-*/
-
-/*
-$a = array(array('p','h'),array('p','h'),'o');
-if(in_array(array('p','h'),$a,true)){
-	echo "find successfully";
-}else{
-	echo "can't find";
-}
-*/
-
-//$food=array('berry','grapes','banana','apple','grapes',55);
- //echo array_search('banana', $food);
-
-//$food = array('a' => 'orange', 'b'=>'banana', 'c'=>'apple','d'=>'grapes');
-
- //echo array_search('apple', $food);
-
-
-//======== array_replace and array_replace_recursive====
-//$fruits = ['apple','a'=>'banana','orange','grapes'];
-
-//$veggie=['a' => 'carrot',1 =>'pea'];
-
-//$colors = ['green','blue','red'];
-
-$array1=array("a"=>array("green"),"b"=>array("blue","red"));
-$array2=array("a"=>array("yellow"),"b"=>array("black"));
-$newArray=array_replace_recursive($array1, $array2);
+array_pop($fruit);
 
 echo "<pre>";
 
-print_r($newArray);
+print_r($fruit);
 
-echo "</pre>"
+echo "</pre>";
+*/
+$fruit = ["orange","banana",];
+
+array_push($fruit,"apple","guava","lemon");
+
+echo "<pre>";
+
+print_r($fruit);
+
+echo "</pre>";
+
+
+
+
 
 
 
