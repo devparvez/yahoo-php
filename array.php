8b16039c8229333624ba95be3349a541cb9f32d9
parvezmahmud-developer <parvezmahmud78@gmail.com>
2021-01-28 09:03:14 +0600

    Array_merge, Array_merge_recursive,Arrar_combine class43

--- array.php
+++ array.php
@@ -1,24 +1,88 @@
 <?php
+/*
+//======array_merge for index array========//
+$fruit = ['apple','mango','banana','orange'];
 
-//=====array_shift and array_unshift=======
-/*$fruit = array('orange','banana','grapes');
+$veggi =['carrot','pea'];
+$color =['red','green'];
 
-array_shift($fruit);
+$newArray=array_merge($fruit,$veggi,$color);
 
 echo "<pre>";
 
-print_r($fruit);
+print_r($newArray);
+
+echo "</pre>";
+*/
+//======array_merge for associative with index array========//
+/*
+$fruit = ['a' => 'apple','b' =>'mango','c' =>'banana','d' =>'orange'];
+
+$veggi =['carrot','pea'];
+$color =['red','green'];
+
+$newArray=array_merge($fruit,$veggi,$color);
+
+echo "<pre>";
+
+print_r($newArray);
 
 echo "</pre>";
 */
 
-$fruit = array('orange','banana','grapes');
+//======array_merge for associative with associative array========//
 
-array_unshift($fruit,'apple','guava','jamrul');
+/*
+$fruit = ['a' => 'apple','b' =>'mango','c' =>'banana','d' =>'orange'];
+
+$veggi =[ 'b' => 'carrot','f' => 'pea'];
+
+$color =['h'=>'red', 'i' => 'green'];
+
+//$newArray=array_merge($fruit,$veggi,$color);
+$newArray = $fruit + $veggi + $color;
 
 echo "<pre>";
 
-print_r($fruit);
+print_r($newArray);
+
+echo "</pre>";
+*/
+
+//======array_merge_recursive for associative with index array========//
+
+//$fruit = ['a' => 'apple','b' =>'mango','c' =>'banana','d' =>'orange'];
+
+//$veggi =[ 'b' => ['color' => ['red','green','blue']],
+
+//'e' => 'pea',55,68];
+
+
+
+//$newArray=array_merge_recursive($fruit,$veggi);
+
+//$newArray = $fruit + $veggi + $color;
+
+//echo "<pre>";
+
+//print_r($newArray);
+
+//echo "</pre>";
+//======array combine=========
+
+$name=['mohon','lal','fazil'];
+
+$age=[25,22,24];
+
+
+
+$newArray=array_combine($name,$age);
+
+
+
+echo "<pre>";
+
+print_r($newArray);
 
 echo "</pre>";
 
